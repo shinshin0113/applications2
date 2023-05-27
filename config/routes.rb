945a2153f751c645267be663d5ba188dbbeb1494
shinshin0113 <130219147+shinshin0113@users.noreply.github.com>
2023-05-27 15:48:43 +0000

    [Finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
-  resources :post_images, only: [:new,:create, :index, :show, :destroy]
+  resources :post_images, only: [:new,:create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:show, :edit, :update]
+  
  
 
 end
